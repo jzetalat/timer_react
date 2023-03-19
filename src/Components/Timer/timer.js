390906f94ba39a697530a9ee69a6c51fe6df97aa
jzetalat <zetalatenejordan@gmail.com>
2023-03-19 16:06:58 +0100

    Project initialization

---
+++ src/Components/Timer/timer.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+
+const Timer = () => {
+
+    useEffect(() => {        
+    });
+
+    return (
+        <div className="timer-container">
+            <h2> Timer Project init</h2>
+        </div>
+    );
+};
+
+export default Timer;
